2d00c1d7f85bdd3190a2939cab47294209221942
liennle14 <liennle14@hotmail.com>
2024-01-21 23:26:57 -0500

    revert

--- deletefile.js
+++
@@ -1,9 +0,0 @@
-fs.unlink('canada.txt', (err) => {
-    if (err) throw err;
-    console.log('canada.txt was deleted');
-})
-
-fs.unlink('us.txt', (err) => {
-    if (err) throw err;
-    console.log('us.txt was deleted');
-})
